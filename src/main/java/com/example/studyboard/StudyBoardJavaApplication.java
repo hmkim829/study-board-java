ea1c8ec9d228dc7a560e528ffc1d27b81b8269c8
hmkim829 <hmkim829@colosseum.kr>
2024-03-14 16:49:55 +0900

    springboot 프로젝트 생성

---
+++ src/main/java/com/example/studyboard/StudyBoardJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyBoardJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyBoardJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyboard/StudyBoardJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBoardJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
