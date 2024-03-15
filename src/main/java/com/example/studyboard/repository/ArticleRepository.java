0f3adcd785ade9f7e58d9af2f5bf9e4abda59c0c
hmkim829 <hmkim829@colosseum.kr>
2024-03-15 15:46:26 +0900

    hal explorer 추가

--- src/main/java/com/example/studyboard/repository/ArticleRepository.java
+++ src/main/java/com/example/studyboard/repository/ArticleRepository.java
@@ -2,8 +2,9 @@ package com.example.studyboard.repository;
 
 import com.example.studyboard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
