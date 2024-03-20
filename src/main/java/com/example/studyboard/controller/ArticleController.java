8b00de983632bce3d58205de5966791d27f37402
hmkim829 <hmkim829@colosseum.kr>
2024-03-20 16:29:41 +0900

    게시판 페이지, 게시글 페이지 테스트코드 수정

--- src/main/java/com/example/studyboard/controller/ArticleController.java
+++ src/main/java/com/example/studyboard/controller/ArticleController.java
@@ -21,7 +21,7 @@ public class ArticleController {
     @GetMapping("/{articleId}")
     public String articles(@PathVariable("articleId") Long articleId,
                            ModelMap map) {
-        map.addAttribute("articles", null);
+        map.addAttribute("article", "article");
         map.addAttribute("articleComments", List.of());
         return "articles/detail";
     }
